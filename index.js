fb3c0b8f4e1c1fde3d0ad71bc0270a795f68f058
Julian Gruber <julian@juliangruber.com>
2017-04-06 17:14:32 +0200

    standard

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = ({ height, width }) => {
 
   return html`
     <g>
-      <line x1=${width/2} y1=0 x2=${width/2} y2=${height} stroke="${main}" />
-      <path d="M0 0 H${width} V${width/2} L${width/2} ${width} L0 ${width/2} Z" fill="${darker}" stroke="black" stroke-width="0.5">
+      <line x1=${width / 2} y1=0 x2=${width / 2} y2=${height} stroke="${main}" />
+      <path d="M0 0 H${width} V${width / 2} L${width / 2} ${width} L0 ${width / 2} Z" fill="${darker}" stroke="black" stroke-width="0.5">
     </g>
   `
 }
