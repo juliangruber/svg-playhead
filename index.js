6fe7082fc7bab0b2c4c3f93d1f29dc61127d65ea
Julian Gruber <julian@juliangruber.com>
2017-04-06 17:37:57 +0200

    style

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const html = require('bel')
 
 module.exports = ({ height, width }) => {
-  const main = 'hsl(60, 100%, 80%)'
-  const darker = 'hsl(60, 80%, 65%)'
+  const main = 'hsl(0, 0%, 100%)'
+  const darker = 'hsla(0, 0%, 90%, 0.8)'
 
   return html`
     <g>
-      <line x1=${width / 2} y1=0 x2=${width / 2} y2=${height} stroke="${main}" />
+      <rect x=${width / 2 - 1} y=${width / 4} width=2 height=${height - width / 4} fill="${main}" stroke="black"/>
       <path d="M0 0 H${width} V${width / 2} L${width / 2} ${width} L0 ${width / 2} Z" fill="${darker}" stroke="black" stroke-width="0.5">
     </g>
   `
